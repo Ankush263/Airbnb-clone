62140c77894e2ba107e9e12d4c60195b81f149de
Ankush263 <ankushbanik263@gmail.com>
2022-07-11 15:25:04 +0530

    Add Banner.jsx

---
+++ src/Banner.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import Button from '@mui/material/Button';
+import './Banner.css';
+import Search from './Search';
+
+const Banner = () => {
+
+  const [showSearch, setShowSearch] = useState(false);
+
+  return (
+    <div className='banner'>
+      <div className="banner__search">
+        {showSearch && <Search />}
+        <Button onClick={() => setShowSearch(!showSearch)} className='banner__searchButton' varient="outlined">Search Dates</Button>
+      </div>
+      <div className="banner__info">
+        <h1>Get Out and stretch your imagination</h1>
+        <h5>Plane a different kind of getaway to uncover the hidden gems near you</h5>
+        <Button variant="contained">
+        Explor Nearby
+        </Button>
+      </div>
+    </div>
+  )
+}
+
+export default Banner;
+
+//1:30:55
